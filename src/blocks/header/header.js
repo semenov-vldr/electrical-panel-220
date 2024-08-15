ca3a31fc85ef7572b016690146cab451195d8496
Vladimir <vova2066@yandex.ru>
2024-08-16 01:34:23 +0300

    add feedback

---
+++ src/blocks/header/header.js
@@ -0,0 +1,32 @@
+function mobileNav () {
+  const header = document.querySelector("header.header");
+  if (!header) return;
+  const nav = header.querySelector(".header__nav");
+  const burger = header.querySelector(".header__burger");
+  const navLinks = nav.querySelectorAll(".header-nav__link");
+
+  function closeMenu () {
+    nav.classList.remove("js-mobile-nav-open");
+    unblockScrollBody();
+  };
+
+  // Открытие мобильного меню Бургер
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("js-mobile-nav-open");
+    toggleBlockScrollBody();
+
+    // Скрытие меню по клику вне блока
+    if ( nav.classList.contains("js-mobile-nav-open") ) {
+      document.addEventListener("click", (evt) => {
+        if (!evt.target.closest(".header")) closeMenu();
+      });
+    }
+  });
+
+  navLinks.forEach(navLink => {
+    navLink.addEventListener("click", closeMenu);
+  });
+}
+
+mobileNav();
+
