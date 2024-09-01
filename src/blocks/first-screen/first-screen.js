895a065fedb9b50c50de5f2817593a24c64f4016
Vladimir <vova2066@yandex.ru>
2024-09-01 18:22:41 +0300

    add first screen

---
+++ src/blocks/first-screen/first-screen.js
@@ -0,0 +1,32 @@
+const firstScreen = document.querySelector("#first-screen");
+
+if (firstScreen) {
+
+  // отчечаем, что первый экран уже был
+  firstScreen.addEventListener("click", () => {
+    window.localStorage.setItem("firstScreen", "on");
+  });
+
+
+  const firstScreenLS = window.localStorage.getItem('firstScreen');
+
+  window.addEventListener("load", () => {
+    firstScreen.classList.add("js-active");
+     if (firstScreenLS === "on") {
+      unblockScrollBody();
+      firstScreen.remove();
+    }
+  });
+
+  const assembly = firstScreen.querySelector(".first-screen__assembly");
+  assembly.addEventListener("click", hiddenFirstScreen);
+
+  function hiddenFirstScreen () {
+    firstScreen.classList.remove("js-active");
+    unblockScrollBody();
+    setTimeout(() => {
+      firstScreen.remove();
+    }, 1000);
+  };
+
+}
